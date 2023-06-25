6015416964371a773e059dd06ac6bbe9f6273eae
Peyman <hassani.peyman627@gmail.com>
2023-06-25 10:49:42 +0330

    Add new tokens and upate lexer

--- token/token.go
+++ token/token.go
@@ -14,8 +14,14 @@ const (
 	IDENT = "IDENT" // add, foobar, x, y, ...
 	INT   = "INT"
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
