938b61466da257fbc29b122ae392ec33ad779147
Peyman <hassani.peyman627@gmail.com>
2023-09-28 17:40:34 +0330

    Add environment object to repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/peyzor/monkey-interpreter/evaluator"
+	"github.com/peyzor/monkey-interpreter/object"
 	"github.com/peyzor/monkey-interpreter/parser"
 	"io"
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -33,7 +35,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
